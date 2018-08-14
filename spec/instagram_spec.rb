10c5e6cf4dd56aaa3608a3dff529fb0560ff2d1c
Vico Ervanda Putra <vicoerv@gmail.com>
2018-08-14 10:07:04 +0800

    merge

--- spec/instagram_spec.rb
+++ spec/instagram_spec.rb
@@ -2,56 +2,17 @@ require 'rspec'
 require 'ig_api'
 
 describe 'ig_api' do
-  # it 'should login' do
-  #   account = ig_api::Account.new
-  #
-  #   user = account.login ENV['INSTAGRAM_USER'], ENV['INSTAGRAM_PASSWORD']
-  #   search = user.search_for_user('instagram')
-  #   instagram_id = search.data[:id]
-  #   follow = user.relationship.create instagram_id
-  #   timeline = user.feed.timeline_media
-  #
-  #   unfollow = user.relationship.destroy instagram_id
-  # end
-
-  before(:each) do
-
-    # @user = account.login ENV['INSTAGRAM_USER'], ENV['INSTAGRAM_PASSWORD']
-    # @search = @user.search_for_user('instagram')
-    # @instagram_id = @search.data[:id]
-    # @follow = @user.relationship.create @instagram_id
-    # @unfollow = @user.relationship.destroy @instagram_id
-    # @timeline = @user.feed.timeline_media
-
-  end
-
   it 'should login' do
     # p IgApi::Media.get_id_from_code 'BlvwDHwFSgy'
     account = IgApi::Account.new
     @user = account.using ENV['INSTAGRAM_SESSION']
-    # @search = @user.info_by_name'vicoerv'
-    #
-    # p @search
-    # @user_id = @search.data[:id]
 
-    # p @user.feed.story [@user_id]
+    expect(@user).to be_instance_of IgApi::User.class
+
+    @search = @user.info_by_name'vicoerv'
+    @user_id = @search.data[:id]
 
     media = IgApi::Media.new(@user)
     likes = media.like('1735389257830282125_1626347005')
-
-    p likes
-    # @user.is_a? IgApi::User.class
   end
-  #
-  # it 'should search' do
-  #   @search.is_a? IgApi::User.class
-  # end
-  #
-  # it 'should instagram id' do
-  #   @instagram_id === 25025320
-  # end
-  #
-  # it 'should follow' do
-  #   p @follow
-  # end
 end
