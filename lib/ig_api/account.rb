f40d0689691a9853476c60a174fd623dc705fc3f
Ryan Kulp <ryanckulp@gmail.com>
2019-03-04 16:34:40 +0800

    support for reading DMs

--- lib/ig_api/account.rb
+++ lib/ig_api/account.rb
@@ -76,5 +76,41 @@ module IgApi
         user_object
       end
     end
+
+    def list_direct_messages(user, limit = 100)
+      base_url = 'https://i.instagram.com/api/v1'
+      rank_token = IgApi::Http.generate_rank_token user.session.scan(/ds_user_id=([\d]+);/)[0][0]
+
+      endpoint = base_url + "/direct_v2/inbox/?persistentBadging=true&use_unified_inbox=true&show_threads=true&limit=#{limit}"
+      param = format('&is_typehead=true&q=%s&rank_token=%s', user.username, rank_token)
+
+      result = api.get(endpoint + param).with(session: user.session, ua: user.useragent).exec
+      result = JSON.parse result.body, object_class: OpenStruct
+
+      # fetch + combine past messages from parent thread
+      all_messages = []
+      result.inbox.threads.each do |thread|
+        # thread_id = thread.thread_v2_id # => 17953972372244048 DO NOT USE V2!
+        thread_id = thread.thread_id # => 340282366841710300949128223810596505168
+        cursor_id = thread.oldest_cursor # '28623389310319272791051433794338816'
+
+        thread_endpoint = base_url + "/direct_v2/threads/#{thread_id}/?cursor=#{cursor_id}"
+        param = format('&is_typehead=true&q=%s&rank_token=%s', user.username, rank_token)
+
+        result = api.get(thread_endpoint + param).with(session: user.session, ua: user.useragent).exec
+        result = JSON.parse result.body, object_class: OpenStruct
+
+        if result.thread && result.thread.items.count > 0
+          older_messages = result.thread.items.sort_by(&:timestamp) # returns oldest --> newest
+          all_messages << {
+            thread_id: thread_id,
+            recipient_username: thread.users.first.username, # possible to have 1+
+            conversations: older_messages << thread.items.first
+          }
+        end
+      end
+
+      all_messages
+    end
   end
 end
